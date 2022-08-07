aa0f0613aa2da4b150830c35d120df3c2fefc395
eliyaaki <69030616+eliyaaki@users.noreply.github.com>
2022-08-07 16:49:01 +0300

    add annotation to EbuyApplication

--- ebuy-backend/src/main/java/com/ebuy/EbuyApplication.java
+++ ebuy-backend/src/main/java/com/ebuy/EbuyApplication.java
@@ -4,11 +4,20 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 //import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 //import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 //@EnableWebMvc
+
+//@ComponentScan(
+//		"com.ebuy"
+//)
+@EnableJpaRepositories("com.ebuy.dao")
+@EntityScan("com.ebuy.data")
 @OpenAPIDefinition(info = @Info(title = "Ebuy API", version = "1.0", description = "Book Store"))
 @SpringBootApplication
 public class EbuyApplication {
